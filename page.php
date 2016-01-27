80d0d21aadfbdb8c4ca691105dd6868a76f80321
blue-baron <blue-baron@users.noreply.github.com>
2016-01-28 10:10:47 +1100

    Revise page.php structure

--- page.php
+++ page.php
@@ -4,25 +4,43 @@
  * @subpackage HTML5-Reset-WordPress-Theme
  * @since HTML5 Reset 2.0
  */
+ 
  get_header(); ?>
+ 
 
-<h1><?php the_title(); ?></h1>
+<h1><?php $permalink = get_permalink($post->post_parent); ?>
+	<a href="<?php echo $permalink; ?>"><?php echo get_the_title( $post->post_parent ); ?></a></h1>
 
-<section class="pageContent lrg-colspan6 med-colspan4 colLAST">
+  <?php //Add Yoast Breadcrumbs 
+	if ( function_exists('yoast_breadcrumb') ) {
+	  yoast_breadcrumb('<p id="breadcrumbs">','</p>');
+	  } ?>
+      
+     
+    <section class="pageContent lrg-colspan6 med-colspan4 colLAST">
+       
+   
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <!-- The Query for any page text -->
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			
 		<article class="post" id="post-<?php the_ID(); ?>">
-
-				<div class="entry">
+				                
+                <div class="productInfo">
 				<?php the_content(); ?>
 				</div>
 
-			</article>
-		
+				</article>
 
-		<?php endwhile; endif; ?>
+		<?php endwhile; endif; ?> 
+        
 
+
+        
+       
+
+
+       
 </section><!-- end .pageContent -->
 
 <?php get_footer(); ?>
